486c248b58a514fb8a97a45a6cd25d08285c9c26
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-31 23:22:35 +0300

    Most water Container

---
+++ Most water/Challange.js
@@ -0,0 +1,18 @@
+var maxArea = function(height) {
+    let n = height.length;
+    let left = 0, right = n - 1;
+    let max_area = 0;
+    while (left < right) {
+        let area = Math.min(height[left], height[right]) * (right - left);
+        max_area = Math.max(max_area, area);
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return max_area;
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxArea([1,1]))
