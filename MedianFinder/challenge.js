162c25a3ec6a72affedf4b7097fdccd1b972f654
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-06-13 16:40:44 +0300

    Median Finder

---
+++ MedianFinder/challenge.js
@@ -0,0 +1,35 @@
+var MedianFinder = function() {
+    this.nums = [];
+    
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+    thi.nums.push(num);
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {
+    let median =0;
+    let length= this.nums.length
+    let sum= this.nums.reduce((current, prev) => total = current + prev, 0);
+    this.nums.forEach((num) =>{
+        if(Nums.length % 2 == 0){
+                 median = sum/length;
+        }
+        return nums[length-1/2];
+    });
+    
+};
+
+/** 
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
--- Two-Sum/challnage.js
+++ Two-Sum/challnage.js
@@ -19,7 +19,7 @@ Algo
 2 Iterate over the array and for each element:
 1.1 Calculate the complement by subtracting the element from the target.
 1.2 Check if the complement exists in the hash map.
-1.3 If the complement exists, return the indices of the current element and the complement.
+1.3 If the complement exists, return the indices of the current element and the idex of complement.
 1.4 If the complement does not exist, add the element and its index to the hash map.
 3 If no pair is found, return null or an empty result.
 
